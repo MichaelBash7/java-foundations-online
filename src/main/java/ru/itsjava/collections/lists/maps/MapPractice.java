4a8133aac84200d8eed41e82a31ad6511ad9bc9b
MichaelBash7 <bash-misha@mail.ru>
2021-08-26 22:54:00 +0300

    add IteratorPractice

--- src/main/java/ru/itsjava/collections/lists/maps/MapPractice.java
+++ src/main/java/ru/itsjava/collections/lists/maps/MapPractice.java
@@ -2,6 +2,7 @@ package ru.itsjava.collections.lists.maps;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class MapPractice {
@@ -47,42 +48,124 @@ public class MapPractice {
         Fruit apple = new Fruit("Apple", 37.9);
         Fruit peach = new Fruit("Peach", 25.5);
         Fruit pear = new Fruit("Pear", 15.4);
-        Fruit melon = new Fruit("Melon", 65.7);
+        Fruit watermelon = new Fruit("Watermelon", 65.7);
 
 
         fruitShop.put("Mikhail", strawberry);
         fruitShop.put("Oleg", apple);
         fruitShop.put("Andrey", peach);
         fruitShop.put("Egor", pear);
-        fruitShop.put("Nikita", melon);
+        fruitShop.put("Nikita", watermelon);
 
 
-        System.out.println("fruitShop.get(\"Oleg\") = " + fruitShop.get("Oleg"));
+//        System.out.println("fruitShop.get(\"Oleg\") = " + fruitShop.get("Oleg"));
+//
+//        fruitShop.remove("Egor", pear);
+//
+//        System.out.println("fruitShop.containsKey(\"Mikhail\") = " + fruitShop.containsKey("Mikhail"));
+//        System.out.println("fruitShop.containsValue(pear) = " + fruitShop.containsValue(pear));
+//
+//        System.out.println("-------");
+//        for (String keyCustomer : fruitShop.keySet()) {
+//            System.out.println(keyCustomer + ": " + fruitShop.get(keyCustomer));
+//
+//        }
+//
+//        System.out.println("--------");
+//        for (Fruit values : fruitShop.values()) {
+//            System.out.println(values + ": " + fruitShop.get(values));
+//        }
+//
+//        System.out.println("-------");
+//
+//        for (Map.Entry<String, Fruit> pair : fruitShop.entrySet()){
+//            System.out.println(pair.getKey() + ": " + pair.getValue());
+//        }
 
-        fruitShop.remove("Egor", pear);
+        System.out.println("Iterator practice: ");
 
-        System.out.println("fruitShop.containsKey(\"Mikhail\") = " + fruitShop.containsKey("Mikhail"));
-        System.out.println("fruitShop.containsValue(pear) = " + fruitShop.containsValue(pear));
+        System.out.println("Кол-во покупателей, чье имя больше 5: ");
 
-        System.out.println("-------");
-        for (String keyCustomer : fruitShop.keySet()) {
-            System.out.println(keyCustomer + ": " + fruitShop.get(keyCustomer));
+        Iterator<String> iterator = fruitShop.keySet().iterator();
+        int count = 0;
+        while (iterator.hasNext()) {
+            String keyCustomer = iterator.next();
+            if (keyCustomer.length() > 5) {
+                count++;
+            }
+        }
+        System.out.println(count);
 
+        System.out.println("Кол-во фруктов, которые равны apple: ");
+
+        Iterator<Fruit> valuesIterator = fruitShop.values().iterator();
+        int fruitsCount = 0;
+        while (valuesIterator.hasNext()){
+            Fruit values = valuesIterator.next();
+            if (values.equals(apple)){
+                fruitsCount++;
+            }
+        }
+        System.out.println(fruitsCount);
+
+        System.out.println("Пропустить 2 элемента со значением watermelon: ");
+
+        Iterator<Fruit> valuesElem = fruitShop.values().iterator();
+        int elemCount = 0;
+        while (valuesElem.hasNext()) {
+            Fruit values = valuesElem.next();
+            if (values.getType().equals("Watermelon") && elemCount < 2) {
+                elemCount++;
+            }else{
+                System.out.println(values);
+            }
         }
 
-        System.out.println("--------");
-        for (Fruit values : fruitShop.values()) {
-            System.out.println(values + ": " + fruitShop.get(values));
+        System.out.println("Пропустить элементы начинающиеся на А: ");
+
+        Iterator<Fruit> letterA = fruitShop.values().iterator();
+        int letterCount = 0;
+        while (letterA.hasNext()) {
+            Fruit values = letterA.next();
+            if (values.getType().startsWith("A") && letterCount < 2) {
+                letterCount++;
+            }else{
+                System.out.println(values);
+            }
         }
 
-        System.out.println("-------");
+        System.out.println("Вернуть первый элемент значение которого Watermelon/watermelon/WATERMELOn: ");
 
-        for (Map.Entry<String, Fruit> pair : fruitShop.entrySet()){
-            System.out.println(pair.getKey() + ": " + pair.getValue());
+        Iterator<Fruit> firstElem = fruitShop.values().iterator();
+
+        while (firstElem.hasNext()) {
+            Fruit values = firstElem.next();
+            if (values.getType().equals("Watermelon") || values.getType().equals("WATERMELOn")) {
+                System.out.println(values);
+                break;
+            }
         }
 
+        System.out.println("Вернуть все элементы (фрукты) в другую карту: ");
 
+        Map<String, Fruit> movedFruits = new HashMap<>();
 
+        movedFruits.putAll(fruitShop);
+        System.out.println(movedFruits);
+
+        System.out.println("Вычисляем средний вес всех фруктов: ");
+
+        Iterator<Fruit> averageWeight = fruitShop.values().iterator();
+        double weightCount = 0.0;
+        double sumWeight = 0.0;
+        while (averageWeight.hasNext()) {
+            Fruit values = averageWeight.next();
+            if (values.getWeight() != 0) {
+                sumWeight += values.getWeight();
+                weightCount++;
+            }
+        }
+        System.out.println(sumWeight/weightCount);
 
     }
 }
