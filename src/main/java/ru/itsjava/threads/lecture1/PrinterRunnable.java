73d7c43467ca087460a059c1a08efcc1bce76275
MichaelBash7 <bash-misha@mail.ru>
2021-09-15 21:34:12 +0300

    add ThreadPractice2

--- src/main/java/ru/itsjava/threads/lecture1/PrinterRunnable.java
+++ src/main/java/ru/itsjava/threads/lecture1/PrinterRunnable.java
@@ -1,17 +1,17 @@
-package ru.itsjava.threads;
+package ru.itsjava.threads.lecture1;
 
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 
 @AllArgsConstructor
-public class RunnableHw implements Runnable{
+public class PrinterRunnable implements Runnable{
     private final String message;
     private final long delay;
 
     @SneakyThrows
     @Override
     public void run() {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             Thread.sleep(delay);
             System.out.print(message + " ");
         }
